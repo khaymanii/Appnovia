8691ae025477b85eb8b45a0796edc8999a69cff5
khaymanii <kennethakpo61@gmail.com>
2024-08-15 18:12:24 +0100

    chore cleanup

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -6,13 +6,19 @@ export default function Testimonials() {
     <div className="mt-16">
       <div>
         {" "}
-        <h1
-          className="text-xl sm:text-2xl lg:text-2xl font-bold mb-6 text-center
-"
-        >
+        <h1 className="text-xl sm:text-2xl lg:text-2xl font-bold mb-4 text-center">
           What Our Users Say About Us{" "}
         </h1>
+        <p className="text-sm sm:text-base md:text-lg lg:text-lg mb-4 text-center">
+          These testimonials showcase Deximpli{`'`}s dedication to driving
+        </p>
       </div>{" "}
+      <Image
+        src="/images/testimonial.svg"
+        alt="testimonial image"
+        width={1900}
+        height={1900}
+      />
     </div>
   );
 }
